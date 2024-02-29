d1de7ee65fd4e3cea797276c87f90fcb4614fe7a
asrilHidayat404 <asril.code404@gmail.com>
2024-02-29 12:18:43 +0700

    First commit

--- backend/server.js
+++ backend/server.js
@@ -18,21 +18,6 @@ app.use(
 app.use(bodyParser.urlencoded({ extended: true })); 
 app.use(bodyParser.json())
 
-// app.get("/user-sign-in", (req, res) => {
-//     db.query("select * from user", async (err, result) => {
-//       const data = await cleanRowDataPacket(result)
-//       const compareResults = result.map(row => {
-//         return {
-//           username: row.username,
-//           isPasswordMatch: bcrypt.compareSync("mabappe", row.password)
-//         };
-//       });
-//       console.log(compareResults)
-//       console.log(data)
-//         res.send("Hasil: " + JSON.stringify(data))
-//     })
-// })
-
 app.post("/user-sign-in", SignIn)
 app.post("/user-log-in", Login)
 
